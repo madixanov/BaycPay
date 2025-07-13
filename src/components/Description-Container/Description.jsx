0c4a4cc79eb0b39da1db3ab3e3574db5c5722ee1
Yokub <yoqub.madixanov@outlook.com>
2025-07-13 20:24:52 +0500

    Creating and adding: description container

---
+++ src/components/Description-Container/Description.jsx
@@ -0,0 +1,51 @@
+import { motion } from "framer-motion";
+import hero from "../../assets/images/hero.svg";
+import "./Description.css";
+
+const phrases = [
+    "• Позвольте нам стать частью вашего финансового успеха",
+    "• Мы обеспечим безопасность ваших транзакций",
+    "• Новые финансовые возможности для вашего бизнеса",
+    "• Экспертный подход и надёжная защита",
+    "• Быстрые и безопасные платежные решения",
+    "• BAYC-PAY — ваш финансовый щит",
+];
+
+const Description = () => {
+    return (
+        <div>
+            <div className="description-container">
+                <p className="description-1">
+                    <span>BAYC-PAY</span> — это надёжный партнёр для компаний и частных клиентов, ищущих быстрые способы приёма и отправки платежей по всему миру.
+                </p>
+
+                <img src={hero} alt="hero" />
+
+                <p className="description-2">
+                    Обеспечивая уверенность и безопасность на каждом этапе вашего финансового пути<br /><br />
+                    Мы предлагаем инновационные платежные решения, которые открывают новые возможности для вашего бизнеса и личных финансов. Команда экспертов позаботится о защите ваших интересов и безопасности транзакций.
+                </p>
+            </div>
+
+            <div className="scroll-text-wrapper">
+                <motion.div
+                    className="scroll-track"
+                    animate={{ x: ["0%", "-100%"] }}
+                    transition={{
+                        repeat: Infinity,
+                        duration: 10,
+                        ease: "linear"
+                    }}
+                >
+                    <div className="scroll-content">
+                        {[...phrases, ...phrases].map((text, index) => (
+                            <span key={index}>{text}</span>
+                        ))}
+                    </div>
+                </motion.div>
+            </div>
+        </div>
+    );
+};
+
+export default Description;
--- src/pages/Home/HomeMain.jsx
+++ src/pages/Home/HomeMain.jsx
@@ -1,5 +1,6 @@
 import { lazy } from "react";
 import "./HomeMain.css";
+import Description from "../../components/Description-Container/Description.jsx";
 
 const Welcoming = lazy(() => import("../../components/Welcoming-Container/Welcoming.jsx"));
 const BrandsScroll = lazy(() => import("../../components/Brands-Scroll/BrandsScroll.jsx"));
@@ -9,6 +10,7 @@ const HomeMain = () => {
     <main className="home-main">
       <Welcoming />
       <BrandsScroll />
+      <Description />
     </main>
   );
 }
